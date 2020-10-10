5bd81ded9eb727d5ef6263ebdff1d08f06a0393d
Madhav Anand <madhav1004@icloud.com>
2020-10-10 21:48:37 +0530

    Made compose to functional and some others preferences.

--- src/Components/Admin/Compose/Compose.js
+++ src/Components/Admin/Compose/Compose.js
@@ -1,34 +1,37 @@
- import React from 'react';
+ import React, {useState} from 'react';
+ import './Compose.css'
  import { Editor } from '@tinymce/tinymce-react';
 
- class Compose extends React.Component {
-   handleEditorChange = (content, editor) => {
-     console.log('Content was updated:', content);
-   }
+ function Compose() {
+     const [personOfTheWeek,setPersonOfTheWeek] = useState('')
 
-   render() {
+     const handleEditorChange = (content,editor)=>{
+         setPersonOfTheWeek(content);
+     }
+
      return (
-       <Editor
-       apiKey="kt05vov950skrvbh3p17lldel39wlt96758sq5rmbkk5wq27"
-         initialValue="<p>This is the initial content of the editor</p>"
-         init={{
-            width: 1200,
-           height: 500,
-           menubar: false,
-           plugins: [
-             'advlist autolink lists link image charmap print preview anchor',
-             'searchreplace visualblocks code fullscreen',
-             'insertdatetime media table paste code help wordcount'
-           ],
-           toolbar:
-             'undo redo | formatselect | bold italic backcolor | \
-             alignleft aligncenter alignright alignjustify | \
-             bullist numlist outdent indent | removeformat | help'
-         }}
-         onEditorChange={this.handleEditorChange}
-       />
+         <div className="compose">
+             <Editor
+                 apiKey="kt05vov950skrvbh3p17lldel39wlt96758sq5rmbkk5wq27"
+                 initialValue="<p>Person of the week.</p>"
+                 init={{
+                     width: 1200,
+                     height: 500,
+                     menubar: false,
+                     plugins: [
+                         'advlist autolink lists link image charmap print preview anchor',
+                         'searchreplace visualblocks code fullscreen',
+                         'insertdatetime media table paste code help wordcount'
+                     ],
+                     toolbar://eslint-disable-next-line
+                         'undo redo | formatselect | bold italic backcolor | \
+                         alignleft aligncenter alignright alignjustify | \
+                         bullist numlist outdent indent | removeformat | help'
+                 }}
+                 onEditorChange={handleEditorChange}
+             />
+         </div>
      );
-   }
  }
 
  export default Compose;
