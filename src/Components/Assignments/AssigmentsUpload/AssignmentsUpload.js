ebeac08c265c102194e3fe97e1ac607ceb638c36
Madhav Anand <madhav1004@icloud.com>
2020-10-16 10:14:59 +0530

    need to make drag and drop upload

--- src/Components/Assignments/AssigmentsUpload/AssignmentsUpload.js
+++ src/Components/Assignments/AssigmentsUpload/AssignmentsUpload.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './AssignmentsUpload.css';
+import {Button} from "@material-ui/core";
 
 function AssignmentsUpload() {
+    const [firstName,setFirstName] = useState('')
+    const [lastName,setLastName] = useState('')
+    const [email,setEmail] = useState('')
+    const [files,setFiles] = useState([])
+
+    const handleFileChange = (e)=>{
+        console.log(e.target.files)
+    }
+
+    useEffect(()=>{
+        console.log(files)
+    },[files])
+
     return (
         <div className="assignments-upload">
             <div className="review-heading">
@@ -10,19 +24,21 @@ function AssignmentsUpload() {
                 <p className="dropdown-label">Name</p>
                 <div className="name">
                     <div className="first-name">
-                        <input type="text" placeholder={'First'}/>
+                        <input type="text" placeholder={'First'} value={firstName} onChange={e=>setFirstName(e.target.value)}/>
                     </div>
 
                     <div className="last-name">
-                        <input type="text" placeholder={'Last'}/>
+                        <input type="text" placeholder={'Last'} value={lastName} onChange={e=>setLastName(e.target.value)}/>
                     </div>
                 </div>
 
                 <p className="dropdown-label">Email</p>
-                <input type="text" />
+                <input type="text" value={email} onChange={e=>setEmail(e.target.value)}/>
 
                 <p className="dropdown-label">Upload Files</p>
-                <input type="file" className={'file-upload-input'}/>
+                <input type="file" className={'file-upload-input'} onChange={handleFileChange}/>
+
+                <Button className={'submit-button-assignments'}>Submit</Button>
             </div>
         </div>
     )
